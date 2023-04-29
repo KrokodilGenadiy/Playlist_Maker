edecb3599faf4477e25f5f80e2d4b54ef0785fe9
Aleksei Usevich <a.usevich@andersenlab.com>
2023-04-29 21:36:23 +0200

    completed tasks for sprint 9, except for saving state for edittext, because i'm already using fragment, and i will probably save data in viewmodel in the future

--- app/src/main/java/com/zaus_app/playlistmaker/view/SearchFragment.kt
+++ app/src/main/java/com/zaus_app/playlistmaker/view/SearchFragment.kt
@@ -1,22 +1,61 @@
 package com.zaus_app.playlistmaker.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.zaus_app.playlistmaker.R
+import com.zaus_app.playlistmaker.databinding.FragmentSearchBinding
 
 
 class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            searchQueryEdittext.setText(savedInstanceState?.getString("query"))
+            clearSearchButton.setOnClickListener {
+                searchQueryEdittext.setText("")
+            }
+            val simpleTextWatcher = object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
 
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    clearSearchButton.visibility = clearButtonVisibility(s)
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+
+                }
+            }
+            searchQueryEdittext.addTextChangedListener(simpleTextWatcher)
+        }
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
 
 }
--- app/src/main/java/com/zaus_app/playlistmaker/view/SettingsFragment.kt
+++ app/src/main/java/com/zaus_app/playlistmaker/view/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.zaus_app.playlistmaker.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.zaus_app.playlistmaker.databinding.FragmentSettingsBinding
 
 
@@ -22,11 +25,56 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with (binding) {
+        with(binding) {
             goBack.setOnClickListener {
                 parentFragmentManager.popBackStack()
             }
+            shareContainer.setOnClickListener {
+                shareApp()
+            }
+            helpContainer.setOnClickListener {
+                writeToSupport()
+            }
+            agreementContainer.setOnClickListener {
+                openAgreement()
+            }
         }
+    }
+
+    private fun shareApp() {
+        val courseLink = "https://praktikum.yandex.ru/android/"
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, courseLink)
+        }
+        startActivity(Intent.createChooser(shareIntent, "Поделиться курсом"))
+    }
+
+    private fun writeToSupport() {
+        val email = "example@mail.com"
+        val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+        val body = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, body)
+            type = "message/rfc822"
+        }
+
+        val chooserIntent = Intent.createChooser(intent, "Send email")
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(chooserIntent)
+        } else {
+            Toast.makeText(requireContext(), "No email client found", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun openAgreement() {
+        val url = "https://yandex.ru/legal/practicum_offer/"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 
     override fun onDestroy() {
