407bbe325cf3d3d19cf714681c95336947315cc8
Aleksei Usevich <a.usevich@andersenlab.com>
2023-11-26 20:21:11 +0300

    set initial classes

--- app/src/main/java/com/zaus_app/playlistmaker/view/rv_adapter/TrackAdapter.kt
+++ app/src/main/java/com/zaus_app/playlistmaker/view/rv_adapter/TrackAdapter.kt
@@ -1,4 +1,52 @@
 package com.zaus_app.playlistmaker.view.rv_adapter
 
-class TrackAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.zaus_app.playlistmaker.R
+import com.zaus_app.playlistmaker.data.Track
+import com.zaus_app.playlistmaker.databinding.TrackItemBinding
+
+class TrackAdapter :
+    ListAdapter<Track, TrackAdapter.TrackViewHolder>(TrackDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val binding =
+            TrackItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TrackViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        val track = getItem(position)
+        holder.bind(track)
+    }
+
+    inner class TrackViewHolder(private val binding: TrackItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(track: Track) {
+            binding.apply {
+                trackName.text = track.trackName
+                groupName.text = track.artistName
+                duration.text = track.trackTime
+                Glide.with(root.context)
+                    .load(track.artworkUrl100)
+                    .centerCrop()
+                    .placeholder(R.drawable.placeholder)
+                    .into(poster)
+            }
+        }
+    }
+
+    class TrackDiffCallback : DiffUtil.ItemCallback<Track>() {
+        override fun areItemsTheSame(oldItem: Track, newItem: Track): Boolean {
+            return oldItem.trackName == newItem.trackName
+        }
+
+        override fun areContentsTheSame(oldItem: Track, newItem: Track): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
